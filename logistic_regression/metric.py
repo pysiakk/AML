1f5a43df43b3d9be40f20d47a39c94b79f510705
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-03-08 10:30:47 +0100

    Feature: implemented metrics precision, recall, f1score

--- logistic_regression/metric.py
+++ logistic_regression/metric.py
@@ -6,6 +6,41 @@ def accuracy(labels_true, labels_pred):
     return np.sum(labels_true == labels_pred) / labels_true.shape[0]
 
 
+def true_positive(labels_true, labels_pred):
+    return np.sum(np.logical_and(labels_true == labels_pred, labels_true == 1))
+
+
+def predicted_positive(labels_true, labels_pred):
+    return np.sum(labels_pred == 1)
+
+
+def condition_positive(labels_true, labels_pred):
+    return np.sum(labels_true == 1)
+
+
+def precision(labels_true, labels_pred):
+    pp = predicted_positive(labels_true, labels_pred)
+    if pp == 0:
+        pp = 10**-9
+    return true_positive(labels_true, labels_pred) / pp
+
+
+def recall(labels_true, labels_pred):
+    cp = condition_positive(labels_true, labels_pred)
+    if cp == 0:
+        cp = 10**-9
+    return true_positive(labels_true, labels_pred) / cp
+
+
+def f1score(labels_true, labels_pred):
+    prec = precision(labels_true, labels_pred)
+    rec = recall(labels_true, labels_pred)
+    if prec + rec == 0:
+        return 2*prec*rec / 10**-9
+    else:
+        return 2*prec*rec / (prec + rec)
+
+
 class Metric(Enum):
     def __new__(cls, function, *args):
         obj = object.__new__(cls)
@@ -18,3 +53,6 @@ class Metric(Enum):
         extend_enum(Metric, name, function)
 
     Acc = accuracy,
+    Precision = precision,
+    Recall = recall,
+    F1score = f1score,
--- logistic_regression/tests/test_metric.py
+++ logistic_regression/tests/test_metric.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from sklearn.utils._testing import assert_array_equal
 
-from ..metric import accuracy
+from ..metric import accuracy, precision, recall, f1score
 
 
 @pytest.fixture()
@@ -10,9 +10,32 @@ def y_true():
     return np.array([0, 1, 1, 0, 0])
 
 
-@pytest.mark.parametrize('y_pred, acc',
-                         [(np.array([1, 1, 0, 0, 1]), 0.4),
-                          (np.zeros(5), 0.6),
-                          (np.ones(5), 0.4)])
-def test_accuracy(y_true, y_pred, acc):
+def accuracy_test(y_true, y_pred, acc):
     assert accuracy(y_true, y_pred) == acc
+
+
+def precision_test(y_true, y_pred, prec):
+    assert precision(y_true, y_pred) == prec
+
+
+def recall_test(y_true, y_pred, rec):
+    assert recall(y_true, y_pred) == rec
+
+
+def f1score_test(y_true, y_pred, f1):
+    print(f1)
+    assert f1score(y_true, y_pred) == f1
+
+
+@pytest.mark.parametrize('y_pred, acc, prec, rec',
+                         [(np.array([1, 1, 0, 0, 1]), 0.4, 1/3, 1/2),
+                          (np.zeros(5), 0.6, 0, 0),
+                          (np.ones(5), 0.4, 0.4, 1)])
+def test_metrics(y_true, y_pred, acc, prec, rec):
+    accuracy_test(y_true, y_pred, acc)
+    precision_test(y_true, y_pred, prec)
+    recall_test(y_true, y_pred, rec)
+    if prec + rec == 0:
+        f1score_test(y_true, y_pred, 2 * prec * rec / 10**-9)
+    else:
+        f1score_test(y_true, y_pred, 2 * prec * rec / (prec + rec))
