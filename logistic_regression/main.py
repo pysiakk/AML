46a9f4e4b064f9a58561b870bc6102c0abf320a4
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-03-10 17:21:00 +0100

    Refactor: printing basic results

--- logistic_regression/main.py
+++ logistic_regression/main.py
@@ -1,4 +1,5 @@
 from logistic_regression.classifier import IRLS, SGD, GD
+from logistic_regression.metric import Metric
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -12,17 +13,11 @@ y[y == 2] = 1
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-irls = IRLS(max_iter=100)
-irls.train(X_train, y_train)
-1 / (1 + np.exp(-X @ irls.beta))
-irls.beta
+irls = IRLS(max_iter=100).train(X_train, y_train)
+print(f'IRLS accuracy: {irls.score(X_test, y_test, Metric.Acc)} and beta: {irls.beta}')
 
-gd = GD(learning_rate=0.01)
-gd.train(X_train, y_train)
-1 / (1 + np.exp(-X @ gd.beta))
-gd.beta
+gd = GD(learning_rate=0.01).train(X_train, y_train)
+print(f'GD accuracy: {gd.score(X_test, y_test, Metric.Acc)} and beta: {gd.beta}')
 
-sgd = SGD(learning_rate=0.01)
-sgd.train(X_train, y_train)
-1 / (1 + np.exp(-X @ sgd.beta))
-sgd.beta
+sgd = SGD(learning_rate=0.01).train(X_train, y_train)
+print(f'SGD accuracy: {sgd.score(X_test, y_test, Metric.Acc)} and beta: {sgd.beta}')
