b148936ab3c5e1ef4b8fdd049b161e98daace60f
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-03-24 10:03:57 +0100

    Create imported classes for knn, lda and qda

--- logistic_regression/experiments/compare_classifiers.py
+++ logistic_regression/experiments/compare_classifiers.py
@@ -1,30 +1,21 @@
-from logistic_regression.classifier import IRLS, SGD, GD, MiniBatchGD
+from logistic_regression.classifier import IRLS, SGD, GD, MiniBatchGD, LDA, QDA, KNN
 from logistic_regression.metric import Metric
 import matplotlib.pyplot as plt
 from logistic_regression.stopper import StopCondition
 from logistic_regression.dataset import datasets
 
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
-from sklearn.neighbors import KNeighborsClassifier
-
 kwargs = {'stop_condition': StopCondition.WorseThanWorst,
           'max_iter_no_imp': 100}
-models = [IRLS, SGD, GD, MiniBatchGD, LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis, KNeighborsClassifier]
+models = [IRLS, SGD, GD, MiniBatchGD, LDA, QDA, KNN]
 metrics = [Metric.Acc, Metric.Precision, Metric.Recall, Metric.F1score]
 
 scores = []
-for dataset in datasets:
+for dataset in datasets[:1]:
     for model_number, model in enumerate(models):
-        if model_number < 4:
-            m = model(**kwargs)
-        else:
-            m = model()
+        m = model(**kwargs)
         m.fit(dataset.X_train, dataset.y_train)
         for metric in metrics:
-            if model_number < 4:
-                score = m.score(dataset.X_test, dataset.y_test, metric)
-            else:
-                score = metric.evaluate(dataset.y_test, m.predict(dataset.X_test))
+            score = m.score(dataset.X_test, dataset.y_test, metric)
             scores.append((dataset.name, model.__name__, metric.name, score))
 
 print(scores)
