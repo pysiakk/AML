6aa1fc7bf355f6e5ec859c2792dbc0a162abafcd
pysiakk <karol.pysiak.13@gmail.com>
2021-04-06 14:25:46 +0200

    Changed colinear feature selection to VIF statsmodels

--- logistic_regression/data_prep.py
+++ logistic_regression/data_prep.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 import numpy as np
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from patsy import dmatrices
 
 banknote = pd.read_csv("data/banknote.csv")
 diabetes = pd.read_csv("data/diabetes.csv")
@@ -18,17 +21,29 @@ def reg(X):
     return X
 
 
-def drop_corr(X):
-    X = reg(X)
-    df = pd.DataFrame(X)
-    cor_matrix = df.corr().abs()
-    upper_tri = cor_matrix.where(np.triu(np.ones(cor_matrix.shape),k=1).astype(np.bool))
-    to_drop = [column for column in upper_tri.columns if any(upper_tri[column] > 0.95)]
-    print(to_drop)
+def drop_corr(df):
+    df = pd.DataFrame(reg(df))
+    # cor_matrix = df.corr().abs()
+    # upper_tri = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+    # to_drop = [column for column in upper_tri.columns if any(upper_tri[column] > 0.95)]
+    # print(to_drop)
+    # df_drop = df.drop(to_drop, axis=1)
+    # return df_drop
+    X = df[list(set(df.columns) - {df.columns[-1]})]
+
+    # VIF dataframe
+    vif_data = pd.DataFrame()
+    vif_data["feature"] = X.columns
+
+    # calculating VIF for each feature
+    vif_data["VIF"] = [variance_inflation_factor(X.values, i)
+                       for i in range(len(X.columns))]
+    to_drop = vif_data[vif_data["VIF"] > 13].feature.values
+    print(sorted(to_drop))
+    print(vif_data)
     df_drop = df.drop(to_drop, axis=1)
     return df_drop
 
-    
 banknote_drop = drop_corr(banknote)
 banknote_drop_train, banknote_drop_test = train_test_split(banknote_drop, test_size=0.33, random_state=123)
 banknote_drop_train.to_csv("data/banknote_train.csv")
