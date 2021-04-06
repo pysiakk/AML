38e87f420979ed9304e88caed6182fb84fb0ad88
pysiakk <karol.pysiak.13@gmail.com>
2021-04-06 19:27:47 +0200

    idk why but with this change empty box on a graph does not appears

--- logistic_regression/experiments/compare_classifiers.py
+++ logistic_regression/experiments/compare_classifiers.py
@@ -10,7 +10,12 @@ kwargs = {'stop_condition': StopCondition.WorseThanWorst,
 models = [IRLS, SGD, GD, MiniBatchGD, LDA, QDA, KNN]
 metrics = [Metric.Acc, Metric.Precision, Metric.Recall, Metric.F1score, Metric.R2]
 
-plt.subplots(3, 2, figsize=(9.6, 10.8))
+plt.subplots(3, 2, figsize=(12, 9))
+ax1 = plt.subplot2grid(shape=(2,6), loc=(0,0), colspan=2)
+ax2 = plt.subplot2grid((2,6), (0,2), colspan=2)
+ax3 = plt.subplot2grid((2,6), (0,4), colspan=2)
+ax4 = plt.subplot2grid((2,6), (1,1), colspan=2)
+ax5 = plt.subplot2grid((2,6), (1,3), colspan=2)
 for dataset_number, dataset in enumerate(datasets):
     plt.subplot(3, 2, dataset_number+1)
     for model_number, model in enumerate(models):
@@ -22,6 +27,6 @@ for dataset_number, dataset in enumerate(datasets):
             scores.append(score)
         plt.bar(np.arange(1, len(metrics)+1) + 0.1*(model_number-3), scores, width=0.1, label=model.__name__)
     plt.xticks(np.arange(1, len(metrics) + 1), [metric.name for metric in metrics])
-    plt.legend(loc=3)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.title(label=f'Comparison of classifiers at {dataset.name}')
 plt.show()
