250a98ff7de355f532ca8c1995cc35c3d1da563c
pysiakk <karol.pysiak.13@gmail.com>
2021-05-14 11:16:36 +0200

    Adding standarizing

--- feature_selection/dataset.py
+++ feature_selection/dataset.py
@@ -5,9 +5,9 @@ import numpy as np
 class Dataset:
     def __init__(self, name):
         self.name = name
-        self.X_train = np.array(pd.read_csv("feature_selection/data/" + name + "_train.data", header=None, sep=" ").iloc[:, :-1])
+        self.X_train = np.array(pd.read_csv("feature_selection/data/" + name + "_train.data", header=None, sep=" ").iloc[:, :-1]) / 999
         self.y_train = (np.array(pd.read_csv("feature_selection/data/" + name + "_train.labels", header=None, sep=" ")) + 1 ) // 2
-        self.X_valid = np.array(pd.read_csv("feature_selection/data/" + name + "_valid.data", header=None, sep=" ").iloc[:, :-1])
+        self.X_valid = np.array(pd.read_csv("feature_selection/data/" + name + "_valid.data", header=None, sep=" ").iloc[:, :-1]) / 999
 
 
 names = ['artificial', "digits"]
