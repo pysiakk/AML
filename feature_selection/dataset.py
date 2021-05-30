86868b3a3411d10af8bea2dc469cf4cb0520c0a9
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-05-30 13:00:48 +0200

    reshape y

--- feature_selection/dataset.py
+++ feature_selection/dataset.py
@@ -7,6 +7,7 @@ class Dataset:
         self.name = name
         self.X_train = np.array(pd.read_csv("feature_selection/data/" + name + "_train.data", header=None, sep=" ").iloc[:, :-1]) / 999
         self.y_train = (np.array(pd.read_csv("feature_selection/data/" + name + "_train.labels", header=None, sep=" ")) + 1 ) // 2
+        self.y_train = self.y_train.reshape(-1)
         self.X_valid = np.array(pd.read_csv("feature_selection/data/" + name + "_valid.data", header=None, sep=" ").iloc[:, :-1]) / 999
 
 
